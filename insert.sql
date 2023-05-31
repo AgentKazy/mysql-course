2914f3f845980d5abd41e2b830826803ac2a01b4
AgentKazy <kazygaming@outlook.pt>
2023-05-31 12:16:48 +0100

    39. Multi-Inserts

--- insert.sql
+++ insert.sql
@@ -1,4 +1,12 @@
 -- CREATE TABLE cats(name VARCHAR(50), age INT);
 -- DESC cats;
 -- INSERT INTO cats (name, age) VALUES ('Link', 5);
+-- INSERT INTO cats (age, name) VALUES (2, 'Riju');
+
+-- INSERT INTO cats (name, age)
+-- VALUES
+--   ('Medoh', 3),
+--   ('Zelda', 4),
+--   ('Tulin', 5);
+
 SELECT * FROM cats;
