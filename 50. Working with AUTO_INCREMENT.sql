78aa96dd94e177fcd9f0b0755cab5752ce5ceb63
AgentKazy <kazygaming@outlook.pt>
2023-06-07 12:36:26 +0100

    50. Working with AUTO_INCREMENT

---
+++ 50. Working with AUTO_INCREMENT.sql
@@ -0,0 +1,10 @@
+-- CREATE TABLE unique_cats3 (
+--   cat_id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     age INT NOT NULL
+-- );
+
+INSERT INTO unique_cats3(name, age) VALUES('Boingo', 1);
+
+SELECT * FROM unique_cats3;
+DESC unique_cats3;
