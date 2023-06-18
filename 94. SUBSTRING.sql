0e9fd0a35b13b3150bf58b3baee0006061dd581b
AgentKazy <kazygaming@outlook.pt>
2023-06-18 20:04:52 +0100

    94. SUBSTRING

---
+++ 94. SUBSTRING.sql
@@ -0,0 +1,8 @@
+-- SELECT SUBSTRING('Hello World', 1, 4);
+-- SELECT SUBSTRING('Hello World', 7);
+-- SELECT SUBSTRING('Hello World', -3);
+-- SELECT title FROM books;
+
+-- SELECT SUBSTRING(title, 1, 15) FROM books;
+-- SELECT * FROM books;
+SELECT SUBSTR(author_lname, 1, 1) AS initial, author_lname FROM books;
