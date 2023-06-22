b1e9d6f7bce748e983b6f5925c0630064c1f0b51
AgentKazy <kazygaming@outlook.pt>
2023-06-22 12:34:27 +0100

    107. Other String Functions

---
+++ 107. Other String Functions.sql
@@ -0,0 +1,27 @@
+-- SELECT
+-- INSERT
+-- ('Hello Bobby!', 6, 0, ' there');
+-- SELECT
+-- INSERT
+--   ('Hello Bobby!', 6, 6, ' there');
+-- ####################
+-- SELECT
+-- LEFT('omghahalol!', 3);
+-- SELECT
+--   RIGHT('omghahalol!', 4);
+-- SELECT
+--   LEFT(author_lname, 1)
+-- FROM
+--   books;
+-- ####################
+-- SELECT
+--   REPEAT('ha', 4);
+-- ####################
+-- SELECT
+--   TRIM('    Lisbon    ');
+SELECT
+  TRIM(
+    LEADING '.'
+    FROM
+      '..........Braga........'
+  );
