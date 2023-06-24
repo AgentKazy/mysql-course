cbbcc7fc7664821a203eec9b136d3ab8950d262b
AgentKazy <kazygaming@outlook.pt>
2023-06-24 11:24:30 +0100

    117. ORDER BY

---
+++ 117. ORDER BY.sql
@@ -0,0 +1,24 @@
+-- SELECT
+--   author_lname
+-- FROM
+--   books
+-- ORDER BY
+--   author_lname;
+-- SELECT
+--   author_lname
+-- FROM
+--   books
+-- ORDER BY
+--   author_lname DESC;
+-- SELECT
+--   title,
+--   pages
+-- FROM
+--   books;
+SELECT
+  title,
+  pages
+FROM
+  books
+ORDER BY
+  released_year;
