ccae62b5400b92cdafaf763f13d218bed13899de
AgentKazy <kazygaming@outlook.pt>
2023-06-25 12:16:54 +0100

    131. Count Basics

---
+++ 131. Count Basics.sql
@@ -0,0 +1,26 @@
+-- SELECT
+--   COUNT(*)
+-- FROM
+--   books;
+-- SELECT
+--   COUNT(author_fname)
+-- FROM
+--   books;
+-- SELECT
+--   COUNT(DISTINCT author_fname)
+-- FROM
+--   books;
+-- SELECT
+--   COUNT(DISTINCT released_year)
+-- FROM
+--   books;
+-- SELECT
+--   COUNT(DISTINCT author_lname)
+-- FROM
+--   books;
+SELECT
+  COUNT(*)
+FROM
+  books
+WHERE
+  title LIKE '%the%';
