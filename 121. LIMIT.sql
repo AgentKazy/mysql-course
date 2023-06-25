4d71692f3cb59b73af9f4e91d424b095e91a3884
AgentKazy <kazygaming@outlook.pt>
2023-06-25 11:15:07 +0100

    121. LIMIT

---
+++ 121. LIMIT.sql
@@ -0,0 +1,18 @@
+-- SELECT
+--   book_id,
+--   title,
+--   released_year
+-- FROM
+--   books
+-- ORDER BY
+--   released_year DESC
+-- LIMIT
+--   5, 10;
+SELECT
+  title
+FROM
+  books
+ORDER BY
+  title
+LIMIT
+  40;
