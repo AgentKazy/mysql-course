15de3d089873e5e3940c723cc3b7ed7a5b836e2c
AgentKazy <kazygaming@outlook.pt>
2023-06-25 12:29:41 +0100

    133. GROUP BY

---
+++ 133. GROUP BY.sql
@@ -0,0 +1,33 @@
+-- SELECT
+--   author_lname
+-- FROM
+--   books
+-- GROUP BY
+--   author_lname;
+-- SELECT
+--   author_lname,
+--   COUNT(*)
+-- FROM
+--   books
+-- GROUP BY
+--   author_lname;
+-- SELECT
+--   author_lname,
+--   COUNT(*) AS books_written
+-- FROM
+--   books
+-- GROUP BY
+--   author_lname
+-- ORDER BY
+--   books_written DESC;
+-- SELECT
+--   released_year,
+--   COUNT(*)
+-- FROM
+--   books
+-- GROUP BY
+--   released_year;
+SELECT
+  *
+FROM
+  books;
