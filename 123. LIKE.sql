9f107aaa10b30401c6bd00d193523108111f70d6
AgentKazy <kazygaming@outlook.pt>
2023-06-25 11:22:40 +0100

    123. LIKE

---
+++ 123. LIKE.sql
@@ -0,0 +1,26 @@
+-- SELECT
+--   title,
+--   author_fname,
+--   author_lname
+-- FROM
+--   books
+-- WHERE
+--   author_fname LIKE '%da%';
+-- SELECT
+--   *
+-- FROM
+--   books
+-- WHERE
+--   title LIKE '%:%';
+-- SELECT
+--   *
+-- FROM
+--   books
+-- WHERE
+--   author_fname LIKE '____';
+SELECT
+  *
+FROM
+  books
+WHERE
+  author_fname LIKE '%n';
