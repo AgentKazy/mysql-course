2bed79ef5282fa7c31f828b3528a79381c6ec58d
AgentKazy <kazygaming@outlook.pt>
2023-06-27 11:18:33 +0100

    156. FLOAT & DOUBLE

---
+++ 156. FLOAT & DOUBLE.sql
@@ -0,0 +1,15 @@
+-- CREATE TABLE nums (x FLOAT, y DOUBLE);
+-- INSERT INTO nums (x,y) VALUES (1.123, 1.123);
+-- INSERT INTO
+--   nums (x, y)
+-- VALUES
+--   (1.12345678, 1.12345678);
+INSERT INTO
+  nums (x, y)
+VALUES
+  (1.12345678, 1.1234567898765432123456789);
+
+SELECT
+  *
+FROM
+  nums;
