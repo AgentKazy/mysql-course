7ee9a55b7eddebfbda2a7e2bdecfbe889a8bf046
AgentKazy <kazygaming@outlook.pt>
2023-06-27 11:07:23 +0100

    155. DECIMAL

---
+++ 155. DECIMAL.sql
@@ -0,0 +1,14 @@
+-- DECIMAL(5,2) (5 Digits, 2 Digits after decimal)
+-- CREATE TABLE products (price DECIMAL(5,2));
+-- INSERT INTO
+--   products (price)
+-- VALUES
+--   (4.50);
+-- INSERT INTO
+--   products (price)
+-- VALUES
+--   (456.99);
+SELECT
+  *
+FROM
+  products;
