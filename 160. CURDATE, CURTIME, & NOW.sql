249c3b1b533ea7e86fdcd464fce0bf3aa89a38e7
AgentKazy <kazygaming@outlook.pt>
2023-06-27 12:07:47 +0100

    160. CURDATE, CURTIME, & NOW

---
+++ 160. CURDATE, CURTIME, & NOW.sql
@@ -0,0 +1,19 @@
+-- SELECT
+--   CURTIME();
+-- SELECT
+--   CURDATE();
+-- SELECT
+--   NOW();
+-- INSERT INTO
+--   people (name, birthdate, birthtime, birthdt)
+-- VALUES
+--   (
+--     'Hazel',
+--     CURDATE(),
+--     CURTIME(),
+--     NOW()
+--   );
+SELECT
+  *
+FROM
+  people;
