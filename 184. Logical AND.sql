9cfe47bbd3070b406df1e11631af215a2610ad71
AgentKazy <kazygaming@outlook.pt>
2023-06-28 12:41:58 +0100

    184. Logical AND

---
+++ 184. Logical AND.sql
@@ -0,0 +1,39 @@
+-- SELECT
+--   *
+-- FROM
+--   books
+-- WHERE
+--   author_lname = 'Eggers';
+-- SELECT
+--   *
+-- FROM
+--   books
+-- WHERE
+--   released_year > 2010;
+-- SELECT
+--   title,
+--   author_lname,
+--   released_year
+-- FROM
+--   books
+-- WHERE
+--   author_lname = 'Eggers'
+--   AND released_year > 2010;
+-- SELECT
+--   title,
+--   author_lname,
+--   released_year
+-- FROM
+--   books
+-- WHERE
+--   author_lname = 'Eggers'
+--   AND released_year > 2010
+--   AND title LIKE '%novel%';
+SELECT
+  title,
+  pages
+FROM
+  books
+WHERE
+  CHAR_LENGTH(title) > 30
+  AND pages > 500;
