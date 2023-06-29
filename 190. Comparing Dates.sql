fcf77c022648b5c5d90cf81c712a0e59068f28ff
AgentKazy <kazygaming@outlook.pt>
2023-06-29 12:29:57 +0100

    190. Comparing Dates

---
+++ 190. Comparing Dates.sql
@@ -0,0 +1,39 @@
+-- SELECT
+--   *
+-- FROM
+--   people;
+-- SELECT
+--   *
+-- FROM
+--   people
+-- WHERE
+--   YEAR(birthdate) < 2005;
+-- SELECT
+--   *
+-- FROM
+--   people
+-- WHERE
+--   HOUR(birthtime) > 12;
+-- SELECT
+--   CAST('09:00:00' AS TIME);
+-- SELECT
+--   *
+-- FROM
+--   people
+-- WHERE
+--   birthtime BETWEEN '12:00:00'
+--   AND '16:00:00';
+-- SELECT
+--   *
+-- FROM
+--   people
+-- WHERE
+--   birthtime BETWEEN CAST('12:00:00' AS TIME)
+--   AND CAST('16:00:00' AS TIME);
+SELECT
+  *
+FROM
+  people
+WHERE
+  HOUR(birthtime) BETWEEN 12
+  AND 16;
