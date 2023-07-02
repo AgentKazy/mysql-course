16507485efcda61ec28e244fec0d3cb88f3b6bc4
AgentKazy <kazygaming@outlook.pt>
2023-07-02 17:37:43 +0100

    202. CHECK Constraints

---
+++ 202. CHECK Constraints.sql
@@ -0,0 +1,21 @@
+-- CREATE TABLE users (
+--   username VARCHAR(20) NOT NULL,
+--   age INT CHECK (age > 0)
+-- );
+-- INSERT INTO users (username, age)
+-- VALUES ('Bluethecat', 50);
+-- INSERT INTO
+--   users (username, age)
+-- VALUES
+--   ('Kazy', 5);
+-- CREATE TABLE palindromes (
+--   word VARCHAR(100) CHECK(REVERSE(word) = word)
+-- );
+-- INSERT INTO
+--   palindromes (word)
+-- VALUES
+--   ('racecar');
+INSERT INTO
+  palindromes (word)
+VALUES
+  ('mommy');
