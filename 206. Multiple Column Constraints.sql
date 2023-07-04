3de38714ecb2e8920d7bf88a1f27a46dd75e9d1b
AgentKazy <kazygaming@outlook.pt>
2023-07-04 12:53:23 +0100

    206. Multiple Column Constraints

---
+++ 206. Multiple Column Constraints.sql
@@ -0,0 +1,18 @@
+-- CREATE TABLE companies (
+--   name VARCHAR (255) NOT NULL,
+--   address VARCHAR(255) NOT NULL,
+--   CONSTRAINT name_address UNIQUE (name, address)
+-- );
+-- INSERT INTO companies (name, address) VALUES ('Blackbird Auto', '123 Spruce');
+-- INSERT INTO companies (name, address) VALUES ("Luigi's Pies", '123 Spruce');
+-- INSERT INTO companies (name, address) VALUES ("Luigi's Pies", '123 Spruce');
+-- CREATE TABLE houses (
+--   purchase_price INT NOT NULL,
+--   sale_price INT NOT NULL,
+--   CONSTRAINT sprice_gt_pprice CHECK (sale_price >= purchase_price)
+-- );
+-- INSERT INTO
+--   houses (purchase_price, sale_price)
+-- VALUES
+--   (100, 200);
+-- INSERT INTO houses (purchase_price, sale_price) VALUES (200, 100);
