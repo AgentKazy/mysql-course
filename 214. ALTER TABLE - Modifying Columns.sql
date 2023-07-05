c4a82c23900c75ed75387fc97409b249515135a9
AgentKazy <kazygaming@outlook.pt>
2023-07-05 11:45:38 +0100

    214. ALTER TABLE: Modifying Columns

---
+++ 214. ALTER TABLE - Modifying Columns.sql
@@ -0,0 +1,8 @@
+-- DESC companies;
+-- ALTER TABLE companies MODIFY company_name VARCHAR (100) DEFAULT 'Unknown' NOT NULL;
+-- INSERT INTO
+--   companies (address) VALUES ('2554 Rose Lane');
+SELECT
+  *
+FROM
+  companies;
