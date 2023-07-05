5cbb9e05d6128eef9e5c22d7a9fe89738915bb5b
AgentKazy <kazygaming@outlook.pt>
2023-07-05 11:33:18 +0100

    208. ALTER TABLE: Adding Columns

---
+++ 208. ALTER TABLE - Adding Columns.sql
@@ -0,0 +1,13 @@
+SELECT
+  *
+FROM
+  companies;
+
+-- ALTER TABLE
+--   companies
+-- ADD
+--   COLUMN phone VARCHAR(15);
+-- ALTER TABLE
+--   companies
+-- ADD
+--   COLUMN employee_count INT NOT NULL;
