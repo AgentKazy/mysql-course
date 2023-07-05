f7aefb60575efb95ae6816e609b1ec41218b2e81
AgentKazy <kazygaming@outlook.pt>
2023-07-05 11:49:57 +0100

    216. ALTER TABLE: Constraints

---
+++ 216. ALTER TABLE - Constraints.sql
@@ -0,0 +1,14 @@
+-- SELECT
+--   *
+-- FROM
+--   houses;
+-- ALTER TABLE
+--   houses
+-- ADD
+--   CONSTRAINT positive_pprice CHECK (purchase_price >= 0);
+-- DESC houses;
+-- INSERT INTO
+--   houses (purchase_price, sale_price)
+-- VALUES
+--   (-1, 4);
+-- ALTER TABLE houses DROP CONSTRAINT positive_pprice;
