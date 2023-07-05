430b3c286852de5aee455eee160c42bee26c2bb3
AgentKazy <kazygaming@outlook.pt>
2023-07-05 11:40:46 +0100

    212. ALTER TABLE: Renaming

---
+++ 212. ALTER TABLE - Renaming.sql
@@ -0,0 +1,19 @@
+-- SELECT
+--   *
+-- FROM
+--   companies;
+-- RENAME TABLE companies TO suppliers;
+-- SELECT
+--   *
+-- FROM
+--   suppliers;
+-- ALTER TABLE suppliers RENAME TO companies;
+-- SHOW TABLES;
+-- SELECT * FROM suppliers;
+-- #########################################
+SELECT
+  *
+FROM
+  companies;
+
+-- ALTER TABLE companies RENAME COLUMN name TO company_name;
