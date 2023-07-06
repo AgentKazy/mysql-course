82155f6e1a0aa590f2962d9cf1553b835b75794d
AgentKazy <kazygaming@outlook.pt>
2023-07-06 12:32:31 +0100

    228. Inner Joins With Group By

---
+++ 228. Inner Joins With Group By.sql
@@ -0,0 +1,22 @@
+USE shop;
+
+-- SELECT
+--   first_name,
+--   last_name,
+--   order_date,
+--   amount
+-- FROM
+--   customers
+--   JOIN orders ON orders.customer_id = customers.id;
+SELECT
+  first_name,
+  last_name,
+  SUM(amount) AS total
+FROM
+  customers
+  JOIN orders ON orders.customer_id = customers.id
+GROUP BY
+  first_name,
+  last_name
+ORDER BY
+  total;
