db3dc918e1c7d838d9c6e1973a17e74f07102da1
AgentKazy <kazygaming@outlook.pt>
2023-07-08 10:39:57 +0100

    236. On Delete Cascade

---
+++ 236. On Delete Cascade.sql
@@ -0,0 +1,45 @@
+USE shop;
+
+-- # THIS WON'T WORK, because the foreign key in ORDERS won't be valid.
+-- DELETE FROM
+--   customers
+-- WHERE
+--   last_name = 'George';
+-- ON DELETE CASCADE
+-- DROP TABLE orders;
+-- DROP TABLE customers;
+-- CREATE TABLE customers (
+--   id INT PRIMARY KEY AUTO_INCREMENT,
+--   first_name VARCHAR(50),
+--   last_name VARCHAR(50),
+--   email VARCHAR(50)
+-- );
+-- CREATE TABLE orders(
+--   id INT PRIMARY KEY AUTO_INCREMENT,
+--   order_date DATE,
+--   amount DECIMAL(8, 2),
+--   customer_id INT,
+--   FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE
+-- );
+-- SHOW TABLES;
+-- INSERT INTO
+--   customers (first_name, last_name, email)
+-- VALUES
+--   ('Boy', 'George', 'george@gmail.com'),
+--   ('George', 'Michael', 'gm@gmail.com'),
+--   ('David', 'Bowie', 'david@gmail.com'),
+--   ('Blue', 'Steele', 'blue@gmail.com'),
+--   ('Bette', 'Davis', 'bette@aol.com');
+-- INSERT INTO
+--   orders (order_date, amount, customer_id)
+-- VALUES
+--   ('2016-02-10', 99.99, 1),
+--   ('2017-11-11', 35.50, 1),
+--   ('2014-12-12', 800.67, 2),
+--   ('2015-01-03', 12.50, 2),
+--   ('1999-04-11', 450.25, 5);
+-- # THIS WORKS NOW, because it will delete the orders where customer_id is the ID from customers.
+-- DELETE FROM
+--   customers
+-- WHERE
+--   last_name = 'George';
