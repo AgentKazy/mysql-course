2b0cd04795ef709c15b2c14a16f0acf6c49476b7
AgentKazy <kazygaming@outlook.pt>
2023-07-09 14:19:34 +0100

    292. Introducing Instagram Clone

---
+++ 292. Introducing Instagram Clone.sql
@@ -0,0 +1,17 @@
+-- CREATE DATABASE ig_clone;
+USE ig_clone;
+
+--
+-- CREATE TABLE users(
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   username VARCHAR(255) UNIQUE NOT NULL,
+--   created_at TIMESTAMP DEFAULT NOW()
+-- );
+--
+DESC users;
+
+-- CREATE TABLE photos();
+-- CREATE TABLE comments();
+-- CREATE TABLE likes();
+-- CREATE TABLE tags();
+-- CREATE TABLE follows();
