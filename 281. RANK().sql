7bc6d8f898bc52c8f3af9411323ea4f22999cbd4
AgentKazy <kazygaming@outlook.pt>
2023-07-09 12:52:43 +0100

    281. RANK()

---
+++ 281. RANK().sql
@@ -0,0 +1,19 @@
+USE window_funcs;
+
+SELECT
+  emp_no,
+  department,
+  salary,
+  RANK() OVER(
+    PARTITION BY department
+    ORDER BY
+      salary DESC
+  ) AS dept_salary_rank,
+  RANK() OVER(
+    ORDER BY
+      salary DESC
+  ) AS overall_salary_rank
+FROM
+  employees
+ORDER BY
+  department;
