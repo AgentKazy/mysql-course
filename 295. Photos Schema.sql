f3970d91b551ce891b1968892e752b1552663c7c
AgentKazy <kazygaming@outlook.pt>
2023-07-09 14:26:06 +0100

    295. Photos Schema

---
+++ 295. Photos Schema.sql
@@ -0,0 +1,23 @@
+-- CREATE DATABASE ig_clone;
+USE ig_clone;
+
+--
+-- CREATE TABLE users(
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   username VARCHAR(255) UNIQUE NOT NULL,
+--   created_at TIMESTAMP DEFAULT NOW()
+-- );
+--
+-- CREATE TABLE photos(
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   image_url VARCHAR(255) NOT NULL,
+--   user_id INT NOT NULL,
+--   created_at TIMESTAMP DEFAULT NOW(),
+--   FOREIGN KEY (user_id) REFERENCES users(id)
+-- );
+DESC photos;
+
+-- CREATE TABLE comments();
+-- CREATE TABLE likes();
+-- CREATE TABLE tags();
+-- CREATE TABLE follows();
